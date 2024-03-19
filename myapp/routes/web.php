643eab57c1076b90b7f18d78e6b7f6242051eb39
Saksham Katiyar <115263557+SakshamKatiy@users.noreply.github.com>
2024-03-19 22:29:21 +0530

    route Parameters

--- myapp/routes/web.php
+++ myapp/routes/web.php
@@ -17,12 +17,42 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/post', function () {
-    return "<h2> Post page </h2>";
-});
+// Route::get('/post/{id?}/comment/{commentid?}', function (string $id = null , string $comment = null) {
+// if($id){
+//     return "<h1> Post ID : " . $id . "</h1><h2>". $comment . "</h2>";
+// }else{
+//     return "<h1> No ID Found </h1>";
 
-Route::view('post','/post');
+// }
+// });
 
-Route::get('/post/firstpost',function(){
-    return view('firstpost');
-});
+// Route::get('/post/{id?}', function (string $id ) {
+//     if($id){
+//         return "<h1> Post ID : " . $id . "</h1>";
+//     }else{
+//         return "<h1> No ID Found </h1>";
+    
+//     }
+//     })->whereAlphaNumeric('id');
+
+
+
+Route::get('/post/{id?}', function (string $id ) {
+    if($id){
+        return "<h1> Post ID : " . $id . "</h1>";
+    }else{
+        return "<h1> No ID Found </h1>";
+    
+    }
+    })->whereIn('id',['movie','song','painting']);
+
+// ->where('id','[0-9]+')
+// ->where('id','[a-zA-Z]+')
+// ->where('id','[0-9]+')->whereAlpha('commentid');
+
+
+// Route::view('/post','post');
+
+// Route::get('/post/firstpost',function(){
+//     return view('firstpost');
+// });
